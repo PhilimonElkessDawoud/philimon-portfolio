f380348318e8a6c2ee1dfdc0b74b8eacc96db2ab
PhilimonElkessDawoud <P.E.DAOUD@student.aast.edu>
2026-06-02 17:55:19 +0200

    chore: add automatic environment generation for vercel build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    emailjsServiceId: 'service_rvc97k8',
-    emailjsTemplateId: 'template_pipkir7',
-    emailjsPublicKey: 'hh0fygjAjuROBVOG7',
+    emailjsServiceId: 'xxx',
+    emailjsTemplateId: 'xxx',
+    emailjsPublicKey: 'xxx',
 };
